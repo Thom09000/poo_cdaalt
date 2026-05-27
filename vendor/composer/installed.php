e667f601804c37575372d0eaff36b93e59079d15
Thomas <thomajuscule@gmail.com>
2026-05-27 10:07:53 +0200

    changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mithridatem/cdaalt',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7527f64d8fd132a90b8d90635023f13c6fbb65af',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mithridatem/cdaalt' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7527f64d8fd132a90b8d90635023f13c6fbb65af',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
